5b6474a74a6a3ac4bd3668e237c3f497b370ba24
247Salafi <81990726+247Salafi@users.noreply.github.com>
2021-05-28 23:58:27 +0100

    Add files via upload

---
+++ ui1le.py
@@ -0,0 +1,68 @@
+USSD_CODE = ("FIRST BANK : *894#")
+print(USSD_CODE)
+display = "Please type in your banks ussd code: 894 "
+print(display)
+display_0= "You have entered the code for FirstBank of Nigeria"
+print(display_0)
+list = [ " \n \n1> Quick banking",  "2> Open an account",  "3> Loans", "4> Firstmonie" , "0> To terminate"]
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print (list[4])
+option_1= ' \n \nPlease select an option: 1'
+print(option_1)
+display_1= [' \n \n1> Transfer  money', '2> Airtime ' ,'3> Airtime(others)' ,'4> Enquiry services  ', '5> Data' ,'6> Pay bills' , '7> Next page', '0> To go back to menu' , "0>TO RETURN TO MENU"]
+print (display_1[0])
+print (display_1[1])
+print (display_1[2])
+print (display_1[3])
+print (display_1[4])
+print (display_1[5])
+print(display_1[6])
+print(display_1[7])
+display_4 = " \n \nPlease select  an option : 0"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print(list[4])
+option_2 = '\n \nPlease select an option: 2'
+print (option_2)
+display_7= ' \n \nYou already have a first bank account.\nYou first\n0>To return back to menu'
+print(display_7)
+display_6='\n \nPlease select an option: 0'
+print(display_6)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print(list [4])
+option_3= " \n \nPlease select an option: 3"
+print(option_3)
+list_1=[ " \n \n1.For self" , "2.For others \n0>To retun to menu" ]
+print( list_1[0])
+print ( list_1[1])
+display_4 = "\n \nPlease select an option: 0"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print(list [4])
+display_6= " \n \nPlease select an option: 4"
+print(display_6)
+display_7= ' \nI accept all the terms and conditions  on https//www.firstbanknigeria.com \n\n1> Accept \n2> Decline \n0> To go back to menu'
+print(display_7)
+display_4 = "\n \nPlease  select  an option:0"
+print(display_4)
+print( list[0])
+print ( list[1])
+print(list [2])
+print (list [3])
+print (list [4])
+option_4= " \n \nPlease select an option: 0"
+print(option_4)
+display_11=" \n \nThank you for banking with us \n You first."
+print(display_11)
